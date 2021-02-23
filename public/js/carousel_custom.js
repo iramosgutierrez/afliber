ec4828f20be8eb2af9275f6166d2e0d25df9754e
hlima <limaherlander@gmail.com>
2021-02-23 11:21:18 +0100

    Improved ways to download dataset. Added tooltips to navbar icons

---
+++ public/js/carousel_custom.js
@@ -0,0 +1,20 @@
+/*
+$(document).ready(function(){
+
+    function resizeCarousel() {
+        if($(window).width() < 768) {
+            $('#catalogueCarousel')
+              .removeClass('w-50')
+              .addClass('w-100');
+        } else {
+            $('#catalogueCarousel')
+              .removeClass('w-100')
+              .addClass('w-50');
+        }
+    }
+    resizeCarousel();
+
+    $(window).resize(resizeCarousel);
+
+});
+*/
